c063ea0e27c0233ed9a8685a6535f4b006d0df20
lqy407763361 <407763361@qq.com>
2025-09-08 17:36:36 +0800

    0908

--- l-blog-common/src/main/java/com/lblog/common/util/GetClientIpUtil.java
+++ l-blog-common/src/main/java/com/lblog/common/util/GetClientIpUtil.java
@@ -1,20 +1,22 @@
 package com.lblog.common.util;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
 
 public class GetClientIpUtil {
     public static String GetClientIp(HttpServletRequest request){
-        String ip = request.getHeader("X-Forwarded-For");
-        if(ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)){
+        String ip = null;
+        ip = request.getHeader("X-Forwarded-For");
+        if(StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)){
             ip = request.getHeader("Proxy-Client-IP");
         }
-        if(ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)){
+        if(StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)){
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
-        if(ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)){
+        if(StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)){
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
         }
-        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)){
+        if(StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)){
             ip = request.getRemoteAddr();
         }
 
