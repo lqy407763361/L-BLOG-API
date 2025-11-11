71b91c9c18d0d71dfee5842dae8b24b8a444bb94
lqy407763361 <407763361@qq.com>
2025-11-11 17:22:43 +0800

    1111

--- l_blog.sql
+++ l_blog.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80043 (8.0.43)
  File Encoding         : 65001
 
- Date: 06/11/2025 17:03:52
+ Date: 11/11/2025 17:07:08
 */
 
 SET NAMES utf8mb4;
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `l_about`;
 CREATE TABLE `l_about`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '内容',
-  `status` int NULL DEFAULT 1 COMMENT '状态，1=启用，2=禁用',
+  `status` int NOT NULL DEFAULT 1 COMMENT '状态，1=启用，2=禁用',
   `edit_time` int NULL DEFAULT NULL COMMENT '编辑时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
@@ -41,11 +41,11 @@ CREATE TABLE `l_admin`  (
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',
   `salt` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '盐',
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '简介',
-  `status` int NULL DEFAULT NULL COMMENT '状态，1=启用，2=禁用',
+  `status` int NOT NULL DEFAULT 1 COMMENT '状态，1=启用，2=禁用',
   `add_time` int NULL DEFAULT NULL COMMENT '创建时间',
   `edit_time` int NULL DEFAULT NULL COMMENT '编辑时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_admin_group
@@ -55,14 +55,14 @@ CREATE TABLE `l_admin_group`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '群组名称',
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '简介',
-  `status` int NULL DEFAULT NULL COMMENT '状态，1=启用，2=禁用',
+  `status` int NOT NULL DEFAULT 1 COMMENT '状态，1=启用，2=禁用',
   `sort_order` int NULL DEFAULT NULL COMMENT '排序',
   `view_power` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '查看权限',
   `edit_power` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '编辑权限',
   `add_time` int NULL DEFAULT NULL COMMENT '添加时间',
   `edit_time` int NULL DEFAULT NULL COMMENT '编辑时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_admin_login_record
@@ -74,7 +74,7 @@ CREATE TABLE `l_admin_login_record`  (
   `login_ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '登录IP',
   `login_time` int NULL DEFAULT NULL COMMENT '登录时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_admin_refresh_token
@@ -84,11 +84,11 @@ CREATE TABLE `l_admin_refresh_token`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `admin_id` int NULL DEFAULT NULL COMMENT '管理员表ID',
   `refresh_token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '长期token',
-  `is_revoked` int NULL DEFAULT NULL COMMENT '是否废弃，0=默认，1=废弃',
+  `is_revoked` int NOT NULL DEFAULT 0 COMMENT '是否废弃，0=默认，1=废弃',
   `add_time` int NULL DEFAULT NULL COMMENT '添加时间',
   `edit_time` int NULL DEFAULT NULL COMMENT '编辑时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_admin_rsa_key
@@ -101,7 +101,7 @@ CREATE TABLE `l_admin_rsa_key`  (
   `private_key_base64` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '私钥Base64编码',
   `add_time` int NULL DEFAULT NULL COMMENT '添加时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_article
@@ -113,12 +113,12 @@ CREATE TABLE `l_article`  (
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '标题',
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '内容',
   `read_count` int NULL DEFAULT NULL COMMENT '阅读量',
-  `status` int NULL DEFAULT NULL COMMENT '状态，1=启用，2=禁用',
+  `status` int NOT NULL DEFAULT 1 COMMENT '状态，1=启用，2=禁用',
   `sort_order` int NULL DEFAULT NULL COMMENT '排序',
   `add_time` int NULL DEFAULT NULL COMMENT '添加时间',
   `edit_name` int NULL DEFAULT NULL COMMENT '编辑时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_article_category
@@ -128,12 +128,12 @@ CREATE TABLE `l_article_category`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '文章分类名称',
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '简介',
-  `status` int NULL DEFAULT NULL COMMENT '状态，1=启用，2=禁用',
+  `status` int NOT NULL DEFAULT 1 COMMENT '状态，1=启用，2=禁用',
   `sort_order` int NULL DEFAULT NULL COMMENT '排序',
   `add_time` int NULL DEFAULT NULL COMMENT '添加时间',
   `edit_time` int NULL DEFAULT NULL COMMENT '编辑时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_message
@@ -144,11 +144,11 @@ CREATE TABLE `l_message`  (
   `user_id` int NOT NULL COMMENT '发送人ID',
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '标题',
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '内容',
-  `is_read` int NULL DEFAULT NULL COMMENT '是否阅读，1=已读，0=未读',
+  `is_read` int NOT NULL DEFAULT 0 COMMENT '是否阅读，1=已读，0=未读',
   `add_time` int NULL DEFAULT NULL COMMENT '添加时间',
   `edit_time` int NULL DEFAULT NULL COMMENT '编辑时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_site_config
@@ -181,13 +181,13 @@ CREATE TABLE `l_user`  (
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户名',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',
   `salt` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '盐',
-  `status` int NULL DEFAULT NULL COMMENT '状态，1=启用，2=禁用',
+  `status` int NOT NULL DEFAULT 1 COMMENT '状态，1=启用，2=禁用',
   `register_type` int NULL DEFAULT 1 COMMENT '注册方式，1=网页注册',
   `register_ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '注册地IP',
   `add_time` int NULL DEFAULT NULL COMMENT '添加时间',
   `edit_time` int NULL DEFAULT NULL COMMENT '编辑时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_user_refresh_token
@@ -195,14 +195,14 @@ CREATE TABLE `l_user`  (
 DROP TABLE IF EXISTS `l_user_refresh_token`;
 CREATE TABLE `l_user_refresh_token`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NULL DEFAULT NULL COMMENT '用户表ID',
+  `user_id` int NOT NULL COMMENT '用户表ID',
   `refresh_token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '长期token',
-  `is_revoked` int NULL DEFAULT NULL COMMENT '是否废弃，0=默认，1=废弃',
+  `is_revoked` int NOT NULL DEFAULT 0 COMMENT '是否废弃，0=默认，1=废弃',
   `add_ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '添加时的IP（登录IP）',
   `add_time` int NULL DEFAULT NULL COMMENT '添加时间',
   `edit_time` int NULL DEFAULT NULL COMMENT '编辑时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_user_rsa_key
@@ -215,7 +215,7 @@ CREATE TABLE `l_user_rsa_key`  (
   `private_key_base64` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '私钥Base64编码',
   `add_time` int NULL DEFAULT NULL COMMENT '添加时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for l_user_visit_record
@@ -223,7 +223,7 @@ CREATE TABLE `l_user_rsa_key`  (
 DROP TABLE IF EXISTS `l_user_visit_record`;
 CREATE TABLE `l_user_visit_record`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NULL DEFAULT NULL COMMENT '用户表ID',
+  `user_id` int NOT NULL COMMENT '用户表ID',
   `visit_module` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '访问模块',
   `visit_ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '访问IP',
   `visit_time` int NULL DEFAULT NULL COMMENT '访问时间',
